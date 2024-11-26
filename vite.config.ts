17178b816a6eab011f0155006da9e8210b96fbfc
rhtjddls123 <jjjk0605@naver.com>
2024-11-26 22:17:21 +0900

    feat: PWA기능 구현

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,63 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/(.*)/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "google-fonts",
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              }
+            }
+          }
+        ]
+      },
+      manifest: {
+        name: "safeWater",
+        short_name: "safeWater",
+        description: "A Progressive Web App built with Vite and React",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/safeWater_any_192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "maskable"
+          },
+          {
+            src: "/safeWater_any_192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any"
+          },
+          {
+            src: "/safeWater_any_512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "maskable"
+          },
+          {
+            src: "/safeWater_any_512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any"
+          }
+        ]
+      }
+    })
+  ],
   server: {
     proxy: {
       "/waternow": {
